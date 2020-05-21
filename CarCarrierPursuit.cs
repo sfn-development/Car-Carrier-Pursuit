def18ce7dee823c41eb77063d5d6c78863524f37
Syed Fahad Nadeem <syedfnadeem323@gmail.com>
2020-05-21 13:15:51 -0400

    Skeleton of callout.

--- CarCarrierPursuit.cs
+++ CarCarrierPursuit.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using CitizenFX.Core;
+using CalloutAPI;
+
 
 namespace Car_Carrier_Pursuit
 {
-    public class CarCarrierPursuit
+
+    [GuidAttribute("9d3946b1-370d-475e-9b70-99263f12ebd9")]
+    [CalloutProperties("Car Carrier Pursuit", "SFN-Development", "1.0.0", Callout.Probability.Medium)]
+    public class CarCarrierPursuit : CalloutAPI.Callout
     {
+
+        public CarCarrierPursuit() 
+        { 
+        
+        
+        
+        }
+
+        public override Task Init()
+        {
+
+            return base.Init();
+
+        }
+        public override void OnStart(Ped closest)
+        {
+
+            base.OnStart(closest);
+
+        }
+
     }
 }
---
+++ compile.bat
@@ -0,0 +1,1 @@
+csc -target:library CarCarrierPursuit.cs -reference:CalloutAPI.net.dll -reference:CitizenFX.Core.dll -out:CarCarrierPursuit.net.dll
