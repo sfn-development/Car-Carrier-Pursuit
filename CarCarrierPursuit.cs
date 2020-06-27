8380deebc31cf0361d05cc4ee59772f0a9f7e462
SFN-Development <sfndevelopment@gmail.com>
2020-06-26 21:24:08 -0400

    Major Changes + Added Destination for car carrier + Changed driving style so they dont constantly ram into the back of people's cars. This also causes very unpredictable driving.
    Minor Changes
    + Michael is now the only ped that spawns
    + Driver spawns with a weapon and will not surrender.

--- CarCarrierPursuit.cs
+++ CarCarrierPursuit.cs
@@ -3,17 +3,20 @@ using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 using CitizenFX.Core;
 using FivePD.API;
-using System.Collections.Generic;
+using CitizenFX.Core.Native;
 
 namespace Car_Carrier_Pursuit
 {
 
     [GuidAttribute("9d3946b1-370d-475e-9b70-99263f12ebd9")]
-    [CalloutProperties("Car Carrier Pursuit", "SFN-Development", "1.0.1")]
+    [CalloutProperties("Car Carrier Pursuit", "SFN-Development", "1.0.2")]
     public class CarCarrierPursuit : FivePD.API.Callout
     {
 
-        //Initalize Global Location and Heading Dictionaries
+        Boolean pursuit = true;
+        Boolean destinationSpawn = false;
+
+        //Initalize global location and heading object
         readonly Locations locations;
 
         /*Truck Variables*/
@@ -24,13 +27,27 @@ namespace Car_Carrier_Pursuit
         Model carCarrier = new Model(VehicleHash.TR4);
         Vehicle trailer;
 
-        /*Ped Variables*/
-        Ped suspect;
+        /*Ped Variables - Person driving the truck*/
+        Ped driver;
+
+        /*Other Suspect - Gang that is waiting for the car trailer at the destination*/
+        
+        //Cars
+        Model dubsta2 = new Model(VehicleHash.Dubsta2);
+        Vehicle car1;
+        Vehicle car2;
+
+        //Peds
+        Ped ped1 = null;
+        Ped ped2;
+        Ped ped3;
+        Ped ped4;
+
 
         public CarCarrierPursuit() 
         {
-            locations = new Locations("ls", 1);
-            InitInfo(locations.truckCords);
+            locations = new Locations("ls", 1, 1);
+            InitInfo(locations.TruckCords);
             this.ShortName = "Car Carrier Pursuit";
             this.CalloutDescription = "There are reports of someone trying to steal a car-carrier from the Premium Deluxe Motorsport's car dealership. Stop the suspects and return the truck and cars to the dealership.";
             this.ResponseCode = 3;
@@ -38,65 +55,128 @@ namespace Car_Carrier_Pursuit
             
         }
 
+        //When user accepts callout
         public async override Task OnAccept()
         {
             this.InitBlip(15, BlipColor.Red);
-            truck = await World.CreateVehicle(phantom, locations.truckCords, locations.truckHeading);
-            trailer = await World.CreateVehicle(carCarrier, locations.trailerCords, locations.trailerHeading);
+            truck = await World.CreateVehicle(phantom, locations.TruckCords, locations.TruckHeading);
+            trailer = await World.CreateVehicle(carCarrier, locations.TrailerCords, locations.TrailerHeading);
 
             CitizenFX.Core.Native.API.AttachVehicleToTrailer(truck.GetHashCode(), trailer.GetHashCode(), 1F);
-            suspect = await SpawnPed(GetRandomPed(), locations.pedCords, locations.pedHeading);
-            suspect.AlwaysKeepTask = true;
+            driver = await SpawnPed(PedHash.Michael, locations.DriverCords, locations.DriverHeading);
             Notify("There are reports of a subject breaking into a car carrier.");
             Notify("Get to the scene and investigate the area.");
 
         }
+
+        //When user arrives on scene
         public async override void OnStart(Ped closest)
         {
 
             /*Dispatch Notification*/
             Notify("The truck has a tracker on it. See your map for location details.");
 
-            /*Cords for person to drive to.*/
-            Random rand = new Random();
-            float xoff = rand.Next(1000, 2000);
-            float yoff = rand.Next(1000, 2000);
-
-            Debug.WriteLine(xoff.ToString() + " " + yoff.ToString());
-
             base.OnStart(closest);
 
-            suspect.Task.EnterVehicle(truck, VehicleSeat.Driver);
+            //All Suspect Tasks
+            driver.Weapons.Give(WeaponHash.Pistol, 100, true, true);
+
+            driver.AlwaysKeepTask = true;
+            driver.Task.EnterVehicle(truck, VehicleSeat.Driver);
             truck.TowVehicle(trailer, false);
-            suspect.DrivingStyle = DrivingStyle.Rushed;
-            suspect.Task.DriveTo(truck, new Vector3(xoff, yoff, 0F), 1000F, 100F, 0);
+            //https://www.vespura.com/fivem/drivingstyle/ is a god send
+            driver.Task.DriveTo(truck, locations.DestinationCords, 0F, 100F, 524844);
 
             Marker.Delete();
             truck.AttachBlip().ShowRoute = true;
+
+            while (pursuit) {
+
+                //Checks if the suspect is within a certain distance of the destination every 4 seconds 
+                await BaseScript.Delay(4000);
+                float distance = truck.Position.DistanceToSquared(locations.DestinationCords);
+                Debug.WriteLine(truck.Speed.ToString());
+                if (distance < 2000F) {
+
+                    pursuit = false;
+                    break;
+                }
+
+                //Check if the truck has stopped
+                if (truck.Speed == 0F) {
+                    await BaseScript.Delay(7000);
+                    if (truck.Speed == 0F) {
+                        pursuit = false;
+                        break; 
+                    }
+                }
+
+                //Spawns destination entities when the pursuit is close to it.
+                if (distance < 50000F && !destinationSpawn) {
+
+                    car1 = await World.CreateVehicle(dubsta2, locations.Car1Cords, locations.Car1Heading);
+                    car2 = await World.CreateVehicle(dubsta2, locations.Car2Cords, locations.Car2Heading);
+
+                    ped1 = await SpawnPed(PedHash.Business01AMM, locations.Ped1Cords, locations.Ped1Heading);
+                    ped1.Weapons.Give(WeaponHash.AssaultRifleMk2,100, true, true);
+                    ped1.AlwaysKeepTask = true;
+                    ped1.Task.GuardCurrentPosition();
+                    ped1.RelationshipGroup = "PRIVATE_SECURITY";
+
+                    ped2 = await SpawnPed(PedHash.Business01AMY, locations.Ped2Cords, locations.Ped2Heading);
+                    ped2.Weapons.Give(WeaponHash.AssaultRifleMk2, 100, true, true);
+                    ped2.AlwaysKeepTask = true;
+                    ped2.Task.GuardCurrentPosition();
+                    ped2.RelationshipGroup = "PRIVATE_SECURITY";
+
+                    ped3 = await SpawnPed(PedHash.FbiSuit01, locations.Ped3Cords, locations.Ped3Heading);
+                    ped3.Weapons.Give(WeaponHash.AssaultRifleMk2, 100, true, true);
+                    ped3.AlwaysKeepTask = true;
+                    ped3.Task.GuardCurrentPosition();
+                    ped3.RelationshipGroup = "PRIVATE_SECURITY";
+
+                    ped4 = await SpawnPed(PedHash.FbiSuit01Cutscene, locations.Ped4Cords, locations.Ped4Heading);
+                    ped4.Weapons.Give(WeaponHash.AssaultRifleMk2, 100, true, true);
+                    ped4.AlwaysKeepTask = true;
+                    ped4.Task.GuardCurrentPosition();
+                    ped4.RelationshipGroup = "PRIVATE_SECURITY";
+
+                    destinationSpawn = true;
+
+                }
             
-            for (int i = 0; i < rand.Next(4,10); i++) {
-                await BaseScript.Delay(rand.Next(120000,180000));
-                xoff = rand.Next(2001, 6000);
-                yoff = rand.Next(2001, 6000);
-
-                if (rand.Next(1, 2) == 1) {
-                    xoff = xoff * -1;
-                }
-                if (rand.Next(1, 2) == 1)
-                {
-                    yoff = yoff * -1;
-                }
-                suspect.Task.DriveTo(truck, new Vector3(xoff, yoff, 0F), 500F, 120F);
-
-                Debug.WriteLine(xoff.ToString() + " " + yoff.ToString());
             }
+
+            driver.Task.LeaveVehicle(LeaveVehicleFlags.LeaveDoorOpen);
+            driver.Task.ShootAt(closest);
+
+            if (ped1 != null) {
+                ped1.Task.ShootAt(closest);
+                ped2.Task.ShootAt(closest);
+                ped3.Task.ShootAt(closest);
+                ped4.Task.ShootAt(closest);
+            }
+            
+
+
+        }
+
+        public override void OnCancelBefore()
+        {
+            //Deletes all destination peds but will keep the car carrier
+            base.OnCancelBefore();
+            ped1 = null;
+            ped2 = null;
+            ped3 = null;
+            ped4 = null;
+            car1 = null;
+            car2 = null;
         }
 
         public void Notify(string message)
         {
-
             ShowNetworkedNotification(message, "CHAR_CALL911", "CHAR_CALL911", "Dispatch", "Car Carrier Pursuit", 15f);
-
         }
+
     }
 }
--- Locations.cs
+++ Locations.cs
@@ -4,42 +4,110 @@ namespace Car_Carrier_Pursuit
 {
     class Locations
     {
+        /*Spawn Locations*/
         private Vector3 truck_cord;
         private Vector3 trailer_cord;
-        private Vector3 ped_cord;
+        private Vector3 driver_cord;
 
-        private int truck_heading;
-        private int trailer_heading;
-        private int ped_heading;
+        /*Destination Locations*/
+        private Vector3 destination_cord;
+
+        //Car1
+        private Vector3 enemyCar1_cords;
+        private Vector3 ped1_cords;
+        private Vector3 ped2_cords;
+
+        //Car2
+        private Vector3 enemyCar2_cords;
+        private Vector3 ped3_cords;
+        private Vector3 ped4_cords;
 
         /// <summary>
         /// Location object depending on where the callout should take place
         /// </summary>
         /// <param name="city">Choose from ls and sandy</param>
         /// <param name="choice">Choose location in the city</param>
-        public Locations(string city, int choice) {
+        /// <param name="dest">Choose destination location</param>
+        public Locations(string city, int choice, int dest) {
 
-            if (city == "ls" && choice == 1) {
+            /*Spawn Cords and Headings*/
 
+            //Premium Deluxe Power Street
+            if (city == "ls" && choice == 1) 
+            {
                 this.truck_cord = new Vector3(-41.87F, -1076.49F, 26.74F);
                 this.trailer_cord = new Vector3(-32.49F, -1080.07F, 26.66F);
-                this.ped_cord = new Vector3(-44F, -1077.65F, 26.67F);
+                this.driver_cord = new Vector3(-44F, -1077.65F, 26.67F);
 
-                this.truck_heading = 70;
-                this.trailer_heading = truck_heading;
-                this.ped_heading = 347;
+                this.TruckHeading = 70;
+                this.TrailerHeading = TruckHeading;
+                this.DriverHeading = 347;
 
             }
-        
+
+            /*Destination Cords and headings*/
+
+            //Paleto BayView Lodge
+            if (dest == 1) {
+
+                this.destination_cord = new Vector3(-672.09F, 5788.22F, 17.34F);
+
+                //Car 1
+                this.enemyCar1_cords = new Vector3(-716.28F, 5768.2F, 17.07F);
+                this.Car1Heading = 17;
+
+                //Peds for car 1
+                this.ped1_cords = new Vector3(-710.16F, 5786.01F, 17.36F);
+                this.Ped1Heading = 128;
+                this.ped2_cords = new Vector3(-691.67F, 5780.69F, 17.33F);
+                this.Ped2Heading = 92;
+
+                //Car 2
+                this.enemyCar2_cords = new Vector3(-708.72F, 5784.58F, 16.89F);
+                this.Car2Heading = 120;
+
+                //Peds for car 2
+                this.ped3_cords = new Vector3(-718.23F, 5766.41F, 17.61F);
+                this.Ped3Heading = 40;
+                this.ped4_cords = new Vector3(-689.35F, 5762.43F, 17.33F);
+                this.Ped4Heading = 45;
+
+            }
+            //TO-DO add destinations with elseif. 
+
         }
 
-        public Vector3 truckCords { get => truck_cord;}
-        public Vector3 trailerCords { get => trailer_cord; }
-        public Vector3 pedCords { get => ped_cord; }
+        /*Spawn Locations Getters*/
+        public Vector3 TruckCords { get => truck_cord;}
+        public Vector3 TrailerCords { get => trailer_cord; }
+        public Vector3 DriverCords { get => driver_cord; }
+       
+        public int TruckHeading { get; }
+        public int TrailerHeading { get; }
+        public int DriverHeading { get; }
 
-        public int truckHeading { get => truck_heading; }
-        public int trailerHeading { get => trailer_heading; }
-        public int pedHeading { get => ped_heading; }
+        /*Destination Location Getters*/
+        public Vector3 DestinationCords { get => this.destination_cord; }
 
+        //Car1
+        public Vector3 Car1Cords { get => this.enemyCar1_cords; }
+        public int Car1Heading { get; }
+
+        //Peds car 1
+        public Vector3 Ped1Cords { get => this.ped1_cords; }
+        public int Ped1Heading { get; }
+
+        public Vector3 Ped2Cords { get => this.ped2_cords; }
+        public int Ped2Heading { get; }
+
+        //Car 2
+        public Vector3 Car2Cords { get => this.enemyCar2_cords; }
+        public int Car2Heading { get; }
+
+        //Peds car 2
+        public Vector3 Ped3Cords { get => this.ped3_cords; }
+        public int Ped3Heading { get; }
+        public Vector3 Ped4Cords { get => this.ped4_cords; }
+        public int Ped4Heading { get; }
     }
 }
